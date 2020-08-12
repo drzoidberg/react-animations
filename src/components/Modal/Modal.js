a4be05ebe1c143188cb92dae014d34b672a5c20a
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-12 18:19:56 +0200

    🚧 403. Animation Timings

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Transition from 'react-transition-group/Transition';
 
-
 import './Modal.css';
+
+const animationTiming = {
+    enter: 400,
+    exit: 1000
+}
 
 const modal = (props) => {
     return (
         <Transition
-        in={props.show}
-        timeout={300}
-        mountOnEnter
-        unmountOnExit
+            in={props.show}
+            timeout={animationTiming}
+            mountOnEnter
+            unmountOnExit
         >
             {state => {
                 const cssClasses  = ['Modal', state === 'entering'
@@ -18,7 +22,6 @@ const modal = (props) => {
                                                 : state === 'exiting'
                                                     ? 'ModalClosed'
                                                     : null]
-
                 return (
                     <div className={cssClasses.join(' ')}>
                         <h1>A Modal</h1>
